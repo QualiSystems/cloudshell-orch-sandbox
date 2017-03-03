6a0001608e19c8bb20ed365651eabb4058c08995
IngaAl <inga.a@quali.com>
2017-03-03 14:06:23 -0800

    added another test for no storage device

--- sandbox_scripts/environment/setup/tests/test_setup_resources.py
+++ sandbox_scripts/environment/setup/tests/test_setup_resources.py
@@ -50,8 +50,8 @@ class SetupResourcesTests(unittest.TestCase):
     @patch('sandbox_scripts.environment.setup.setup_resources.SandboxBase')
     @patch('sandbox_scripts.environment.setup.setup_resources.NetworkingSaveRestore')
     def test_flow_ok_with_gold(self, mock_save, mock_sandboxbase, mock_api_session):
-
         mock_save.return_value.is_snapshot.return_value = False
+        mock_sandboxbase.return_value.get_storage_server_resource.return_value = True
         self.setup_script.execute()
         mock_sandboxbase.return_value.clear_all_resources_live_status.assert_called_with()
         mock_save.return_value.load_config.assert_called_with(config_set_name='', config_stage='Gold', config_type='Running', ignore_models=['Generic TFTP server', 'Config Set Pool', 'Generic FTP server', 'netscout switch 3912'])
@@ -61,6 +61,17 @@ class SetupResourcesTests(unittest.TestCase):
                              call('Sandbox setup finished successfully')]
         mock_sandboxbase.return_value.report_info.assert_has_calls(report_info_calls)
 
+    @patch('cloudshell.helpers.scripts.cloudshell_scripts_helpers.get_api_session')
+    @patch('sandbox_scripts.environment.setup.setup_resources.SandboxBase')
+    @patch('sandbox_scripts.environment.setup.setup_resources.NetworkingSaveRestore')
+    def test_flow_ok_with_no_storage_device(self, mock_save, mock_sandboxbase, mock_api_session):
+        mock_sandboxbase.return_value.get_storage_server_resource.return_value = False
+        self.setup_script.execute()
+        mock_sandboxbase.return_value.clear_all_resources_live_status.assert_called_with()
+        report_info_calls = [call('Beginning load configuration for resources'),
+                             call('Skipping load configuration. No storage resource associated with the blueprint ', write_to_output_window=True),
+                             call('Sandbox setup finished successfully')]
+        mock_sandboxbase.return_value.report_info.assert_has_calls(report_info_calls)
 
 
 if __name__ == '__main__':
