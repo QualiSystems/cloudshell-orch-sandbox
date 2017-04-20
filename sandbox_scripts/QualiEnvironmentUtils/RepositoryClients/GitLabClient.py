05d21fcc0d7043371dff8a827848b0f2a2bfae4a
jimbr70 <jim.b@qualisystems.com>
2017-04-20 12:09:46 -0700

    import change

--- sandbox_scripts/QualiEnvironmentUtils/RepositoryClients/GitLabClient.py
+++ sandbox_scripts/QualiEnvironmentUtils/RepositoryClients/GitLabClient.py
@@ -2,28 +2,28 @@ from RepositoryClient import *
 from sandbox_scripts.QualiEnvironmentUtils.Sandbox import *
 import base64
 
+try:
+    import gitlab
+    imported_gitlab = True
+except Exception as e:
+    imported_gitlab = False
 
 class GitLabClient(RepositoryClient):
+    # ----------------------------------
+    # ----------------------------------
     def __init__(self, sandbox,repository_resource ):
         super(GitLabClient,self).__init__(sandbox, repository_resource)
-        try:
-            import gitlab
-        except:
-            try:
-                import pip
-                pip.main(['install','python-gitlab'])
-                import gitlab
-            except:
-                self.sandbox.report_error('gitlab lib was not installed', write_to_output_window=True)
+        if imported_gitlab == False:
+            self.sandbox.report_error('gitlab lib was not found', write_to_output_window=True)
         self.url = repository_resource.get_attribute("GitLab URL")
         self.token = repository_resource.get_attribute("GitLab Token")
         self.project_name = repository_resource.get_attribute("GitLab Project Name")
         self.repository_path = repository_resource.get_attribute("Repository Path")
 
+    # ----------------------------------
+    # ----------------------------------
     def download(self, source, destination):
-        #print "GitLabClient called!"
-        #print "   " + source
-        #print "   " + destination
+
         gl = gitlab.Gitlab(self.url, self.token)
         projid = 0
         projects = ''
