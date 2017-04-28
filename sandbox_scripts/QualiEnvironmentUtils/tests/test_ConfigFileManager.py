a2f11f50b66cb01336a0607cdffc93d58d7c6ef3
Yaniv Kalsky <yanivka@gmail.com>
2017-04-28 14:36:13 -0700

    Added another test

--- sandbox_scripts/QualiEnvironmentUtils/tests/test_ConfigFileManager.py
+++ sandbox_scripts/QualiEnvironmentUtils/tests/test_ConfigFileManager.py
@@ -202,6 +202,38 @@ class ConfigFileManagerTests(unittest.TestCase):
                          "Error is: Failed to create a concrete config file from the template's data. "
                          "Unexpected error: Could not find attribute 'Pool123' in resource 'myresource'")
 
+    @patch('cloudshell.helpers.scripts.cloudshell_scripts_helpers.get_api_session')
+    def test_attribute_from_other_resource_in_sandbox_resource_not_found(self, mock_api_session):
+        config_file_mgr = ConfigFileManager()
+
+        rd = Mock()
+        rd.Name = 'myresource'
+        rd.Address = '1.2.3.4'
+        attr1 = Mock()
+        attr1.Name = 'Pool1'
+        attr1.Value = 'Pool1Val'
+        rd.ChildResources = []
+        rd.ResourceAttributes = [attr1]
+        mock_api_session.return_value.GetResourceDetails = Mock(return_value=rd)
+        resource = ResourceBase(resource_name='myresource', resource_alias='OtherDevice')
+
+        sandbox = Mock()
+        sandbox.get_root_networking_resources.return_value = [resource]
+
+        tmp_template_config_file_data = """{Device:OtherDevice2:Pool1}"""
+
+        config_set_pool_data = {}
+        with self.assertRaises(QualiError) as e:
+            concrete_config_data = config_file_mgr.create_concrete_config_from_template(
+                tmp_template_config_file_data, config_set_pool_data, sandbox, resource)
+
+        the_exception = e.exception
+        self.assertEqual(str(the_exception),
+                         "CloudShell error at ConfigFileManager. Error is: "
+                         "Failed to create a concrete config file from the template's data. "
+                         "Unexpected error: Could not find a resource with alias OtherDevice2; "
+                         "likely missing from blueprint.")
+
 
 if __name__ == '__main__':
     unittest.main()
