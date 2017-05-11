ec45e10a67e0bdea3a65a41930608ac3228f1893
Tomer Admon <tomer.a@qualisystems.com>
2017-05-11 19:00:16 +0300

    Update pack.bat

--- pack.bat
+++ pack.bat
@@ -1,5 +1,4 @@
 @echo off
 python -m pip install qpm --no-cache-dir --upgrade
 copy version.txt SandboxOrchestrationPackage/version.txt /Y
-xcopy .\cloudshell-orch-core\* SandboxOrchestration\environment_scripts\env_setup\ /y /s
 python -m qpm pack --package_name SandboxOrchestration
