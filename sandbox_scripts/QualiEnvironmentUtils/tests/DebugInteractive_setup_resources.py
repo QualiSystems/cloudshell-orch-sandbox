6eeeeb4e6e655eb8cceb35bca6c1b405ae3aa58e
jimbr70 <jim.b@qualisystems.com>
2017-06-12 14:32:37 -0700

    pwd chg

--- sandbox_scripts/QualiEnvironmentUtils/tests/DebugInteractive_setup_resources.py
+++ sandbox_scripts/QualiEnvironmentUtils/tests/DebugInteractive_setup_resources.py
@@ -5,7 +5,7 @@ from sandbox_scripts.environment.setup.setup_resources import *
 os.environ["environmentName"] = "Just1Res"
 
 
-dev_helpers.attach_to_cloudshell_as(user="admin", password="dev", domain="Global",
+dev_helpers.attach_to_cloudshell_as(user="admin", password="xxxx", domain="Global",
                                     reservation_id="ad024811-7528-42eb-b2c4-d50003951278",
                                     server_address="svl-dev-quali")
 
