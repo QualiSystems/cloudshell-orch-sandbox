76a040ed936fd71ad1cbe01e2281a5c9c110637e
jimbr70 <jim.b@qualisystems.com>
2017-06-12 14:32:51 -0700

    pwd change

--- sandbox_scripts/QualiEnvironmentUtils/tests/DebugInteractive_teardown_resources.py
+++ sandbox_scripts/QualiEnvironmentUtils/tests/DebugInteractive_teardown_resources.py
@@ -5,7 +5,7 @@ import os
 
 os.environ["environmentName"] = "Just1Res"
 
-dev_helpers.attach_to_cloudshell_as(user="admin", password="dev", domain="Global",
+dev_helpers.attach_to_cloudshell_as(user="admin", password="xxxx", domain="Global",
                                     reservation_id="2bc2e45f-63e6-41cb-a8f5-fe34e042a75e",
                                     server_address="svl-dev-quali")
 
