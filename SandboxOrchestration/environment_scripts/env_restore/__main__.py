9b1a49c3ff60439ac479313c08227b997aeed434
TomerAdmon <tomer.a@quali.com>
2018-06-26 12:14:42 +0300

    restore logic

--- SandboxOrchestration/environment_scripts/env_restore/__main__.py
+++ SandboxOrchestration/environment_scripts/env_restore/__main__.py
@@ -1,5 +1,8 @@
 from cloudshell.workflow.orchestration.sandbox import Sandbox
+from cloudshell.workflow.orchestration.setup.default_setup_orchestrator import DefaultSetupWorkflow
 
 sandbox = Sandbox()
+
+DefaultSetupWorkflow().register(sandbox)
 
 sandbox.execute_restore()
